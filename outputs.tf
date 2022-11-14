735d2ff1ea02a222526e04e045801fa3f4151f8b
babebort <babebort@gmail.com>
2022-11-14 21:01:24 +0000

    feat: feat: added all helm variables

--- outputs.tf
+++ outputs.tf
@@ -4,31 +4,31 @@ output "metadata" {
 }
 
 output "server_service_account_role_arn" {
-  value       = module.server_eks_iam_role.service_account_role_arn
+  value       = module.argocd_server_iam_role.service_account_role_arn
   description = "ArgoCD server IAM role ARN"
 }
 
 output "server_service_account_policy_name" {
-  value       = module.server_eks_iam_role.service_account_policy_name
+  value       = module.argocd_server_iam_role.service_account_policy_name
   description = "ArgoCD server IAM policy name"
 }
 
 output "server_service_account_policy_id" {
-  value       = module.server_eks_iam_role.service_account_policy_id
+  value       = module.argocd_server_iam_role.service_account_policy_id
   description = "ArgoCD server IAM policy ID"
 }
 
 output "application_controller_service_account_role_arn" {
-  value       = module.application_controller_eks_iam_role.service_account_role_arn
+  value       = module.argocd_application_controller_iam_role.service_account_role_arn
   description = "ArgoCD application-controller IAM role ARN"
 }
 
 output "application_controller_service_account_policy_name" {
-  value       = module.application_controller_eks_iam_role.service_account_policy_name
+  value       = module.argocd_application_controller_iam_role.service_account_policy_name
   description = "ArgoCD application-controller IAM policy name"
 }
 
 output "application_controller_service_account_policy_id" {
-  value       = module.application_controller_eks_iam_role.service_account_policy_id
+  value       = module.argocd_application_controller_iam_role.service_account_policy_id
   description = "ArgoCD application-controller IAM policy ID"
 }
