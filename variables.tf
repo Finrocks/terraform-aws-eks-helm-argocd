9d21578bd0e7599a2c82f7c087ed5303ac32885f
babebort <babebort@gmail.com>
2022-11-17 23:01:05 +0000

    fix: count issue

--- variables.tf
+++ variables.tf
@@ -76,7 +76,7 @@ variable "config" {
   type = object({
     create_default_iam_policy  = optional(bool, true)
     create_default_iam_role    = optional(bool, true)
-    iam_policy_document        = optional(list(string))     #optional(string)
+    iam_policy_document        = optional(list(any))     #optional(string)
     use_sts_regional_endpoints = optional(bool, false)
   })
 
