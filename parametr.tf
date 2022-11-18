22a187a1b9a51245ce9b196d028bb05a24077461
babebort <babebort@gmail.com>
2022-11-18 12:15:53 +0000

    fix: parametr store name

--- parametr.tf
+++ parametr.tf
@@ -3,6 +3,8 @@ module "argocd_kms_key" {
   source  = "cloudposse/kms-key/aws"
   version = "0.12.1"
 
+  environment = split("-", local.eks_cluster_id)[0]
+  tenant = split("-", local.eks_cluster_id)[1]
   description             = format("KMS key for Argocd on %s", local.eks_cluster_id)
   deletion_window_in_days = 10
   enable_key_rotation     = true
