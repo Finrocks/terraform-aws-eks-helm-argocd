df931c3bdb20d68901cf85b8952f498a5b00f134
babebort <babebort@gmail.com>
2022-11-18 10:33:42 +0000

    fix: parametr store name

--- label.tf
+++ label.tf
@@ -19,25 +19,29 @@ module "argocd_kms_label" {
   source  = "cloudposse/label/null"
   version = "0.25.0"
 
-  environment = var.environment
+
   label_order = ["namespace", "environment", "stage", "tenant", "name", "attributes"]
   attributes  = ["kms-key"]
   context     = module.argocd_additional_label.context
 }
 
-module "argocd_label" {
-  source  = "cloudposse/label/null"
-  version = "0.25.0"
-
-  environment = var.environment
-  tenant      = var.tenant
-  context     = module.label.context
-}
+#module "argocd_label" {
+#  source  = "cloudposse/label/null"
+#  version = "0.25.0"
+#
+#  environment = var.environment
+#  tenant      = var.tenant
+#  context     = module.label.context
+#}
 
 module "argocd_additional_label" {
   source  = "cloudposse/label/null"
   version = "0.25.0"
 
-  #tenant      = var.tenant
   context     = module.this.context
+}
+
+module "argocd_tenant_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
 }
