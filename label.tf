08b01961a49701c1d058bd7ffbb1a8878fbcf5b4
babebort <babebort@gmail.com>
2022-11-18 07:50:11 +0000

    fix: parametr store name

--- label.tf
+++ label.tf
@@ -11,7 +11,7 @@ module "parameter_store_label" {
   version = "0.25.0"
 
 #  environment = var.environment
-  name = format("%s\/argocd\/password", local.eks_cluster_id)
+  name = format("%s\\/argocd\\/password", local.eks_cluster_id)
   #"/${local.eks_cluster_id}/argocd/password"
   delimiter   = "/"
   label_order = ["namespace", "environment", "stage", "tenant", "name", "attributes"]
