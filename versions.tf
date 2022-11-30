8cd884fd70fa80f9c2f50ccb5cb0e7308669af4b
babebort <89022219+babebort@users.noreply.github.com>
2022-11-30 22:01:09 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,3 +1,22 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2"
+    }
+    utils = {
+      source  = "cloudposse/utils"
+      version = ">= 0.14.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
 }
