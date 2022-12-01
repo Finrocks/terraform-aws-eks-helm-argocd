955c37a224462936e557db925fd3dd4d2e9cb1ec
babebort <89022219+babebort@users.noreply.github.com>
2022-12-01 13:05:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   enabled                                     = module.this.enabled
   account_id                                  = one(data.aws_caller_identity.default[*].account_id)
   eks_cluster_id                              = one(data.aws_eks_cluster.cluster[*].id)
-  argocd_endpoint                             = one(data.aws_eks_cluster.cluster[*].endpoint)
+#  argocd_endpoint                             = one(data.aws_eks_cluster.cluster[*].endpoint)
   eks_cluster_oidc_issuer_url                 = one(data.aws_eks_cluster.cluster[*].identity[0].oidc[0].issuer)
   application_controller_service_account_name = format("%s-application-controller", var.helm_config["name"])
   server_service_account_name                 = format("%s-server", var.helm_config["name"])
