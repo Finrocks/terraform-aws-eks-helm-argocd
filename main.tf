0fbb6dffa6ef4dc066d1b89deb4fa56e7e4b5950
babebort <babebort@gmail.com>
2022-12-05 18:43:36 +0000

    feat: added setup_admin_password

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ locals {
       fullname_override      = var.helm_config["name"]
       sts_regional_endpoints = var.config["use_sts_regional_endpoints"]
       role_enabled           = local.iam_role_enabled
+      setup_admin_password   = var.argocd_config["setup_admin_password"]
       controller_sa_name     = local.application_controller_service_account_name
       controller_role_arn    = local.iam_role_enabled == true ? one(module.argocd_application_controller_iam_role[*].service_account_role_arn) : ""
       server_sa_name         = local.server_service_account_name
       server_role_arn        = local.iam_role_enabled == true ? one(module.argocd_server_iam_role[*].service_account_role_arn) : ""
       argocd_url             = var.argocd_config["argocd_url"]
       admin_password         = one(data.aws_ssm_parameter.encrypted_password[*].value)
-      #admin_password         = module.argocd_parameter_store_read.values
     }
   )
 }
