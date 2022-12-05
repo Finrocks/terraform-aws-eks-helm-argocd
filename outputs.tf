d035c0783024097d55ad3ec7d3a1112311c07d5e
babebort <babebort@gmail.com>
2022-12-05 22:07:38 +0000

    fix: setup_admin_password issue

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "argocd_password" {
 #  value = length(one(random_password.argocd_password[*].result)) > 0 ? nonsensitive(one(random_password.argocd_password[*].result)) : one(random_password.argocd_password[*].result)
-  value = try(nonsensitive(one(random_password.argocd_password[*].result)))
+  value = try(nonsensitive(one(random_password.argocd_password[*].result)), null)
 #  value = nonsensitive(random_password.argocd_password.result)
   description = "Argocd raw password"
 }
