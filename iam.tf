21dde911d73e74396adf8c61bf8b32102f709ddc
babebort <babebort@gmail.com>
2022-12-06 06:10:15 +0000

    feat: merge policy

--- iam.tf
+++ iam.tf
@@ -26,7 +26,7 @@ data "aws_iam_policy_document" "merge" {
   override_policy_documents = [
     one(data.aws_iam_policy_document.argocd[*].json),
 #    local.iam_role_enabled && var.argocd_config["setup_admin_password"] ? one(data.aws_iam_policy_document.kms[*].json) : ""
-    try(one(data.aws_iam_policy_document.kms[*].json), "")
+    length(data.aws_iam_policy_document.kms[*].json) > 0 ? one(data.aws_iam_policy_document.kms[*].json) : ""
   ]
 }
 
