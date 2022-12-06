973420a38e39a3aff8e14430ad2dc9ec84478cb7
babebort <babebort@gmail.com>
2022-12-06 13:33:53 +0000

    feat: argocd additional cluster

--- versions.tf
+++ versions.tf
@@ -29,22 +29,22 @@ terraform {
   }
 }
 
-provider "argocd" {
-  server_addr                 = "argocd-server:80"
-  username                    = "admin"
-  password                    = one(random_password.argocd_password[*].result)
-  insecure                    = true
-  port_forward                = true
-  port_forward_with_namespace = var.helm_config["namespace"]
-
-  kubernetes {
-    host                   = one( data.aws_eks_cluster.cluster[*].endpoint)
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = ["eks", "get-token", "--cluster-name", local.eks_cluster_id]
-      command     = "aws"
-    }
-  }
-}
+#provider "argocd" {
+#  server_addr                 = "argocd-server:80"
+#  username                    = "admin"
+#  password                    = one(random_password.argocd_password[*].result)
+#  insecure                    = true
+#  port_forward                = true
+#  port_forward_with_namespace = var.helm_config["namespace"]
+#
+#  kubernetes {
+#    host                   = one( data.aws_eks_cluster.cluster[*].endpoint)
+#    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#
+#    exec {
+#      api_version = "client.authentication.k8s.io/v1alpha1"
+#      args        = ["eks", "get-token", "--cluster-name", local.eks_cluster_id]
+#      command     = "aws"
+#    }
+#  }
+#}
