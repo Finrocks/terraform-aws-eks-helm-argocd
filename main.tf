37c3bc721824bcd61a4b840db258a073dc2c63c4
babebort <babebort@gmail.com>
2022-12-06 06:38:36 +0000

    feat: merge policy

--- main.tf
+++ main.tf
@@ -19,10 +19,8 @@ locals {
       admin_password_setup                    = local.admin_password_enabled
       controller_sa_name                      = local.application_controller_service_account_name
       controller_role_arn                     = local.iam_role_enabled ? one(module.argocd_application_controller_iam_role[*].service_account_role_arn) : try(one(module.argocd_application_controller_iam_role[*].service_account_role_arn), "")
-#      controller_role_arn                     = local.iam_role_enabled == true ? one(module.argocd_application_controller_iam_role[*].service_account_role_arn) : try(one(module.argocd_application_controller_iam_role[*].service_account_role_arn), "qqq")
       server_sa_name                          = local.server_service_account_name
-      server_role_arn                         = local.iam_role_enabled ? one(module.argocd_server_iam_role[*].service_account_role_arn) : "qwee"
-#      server_role_arn                         = local.iam_role_enabled == true ? try(one(module.argocd_server_iam_role[*].service_account_role_arn), "qwe") : "qwee"
+      server_role_arn                         = local.iam_role_enabled ? one(module.argocd_server_iam_role[*].service_account_role_arn) : ""
       argocd_url                              = var.argocd_config["argocd_url"]
       admin_password                          = try(one(data.aws_ssm_parameter.encrypted_password[*].value), null)
     }
