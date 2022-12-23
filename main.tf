33ab714c2306f8c34ff44af3f7cf71096744f3ec
babebort <babebort@gmail.com>
2022-12-23 04:00:05 +0000

    feat: argocd nginx ingress

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ locals {
       controller_role_arn                     = local.iam_role_enabled ? one(module.argocd_application_controller_iam_role[*].service_account_role_arn) : try(one(module.argocd_application_controller_iam_role[*].service_account_role_arn), "")
       server_sa_name                          = local.server_service_account_name
       server_role_arn                         = local.iam_role_enabled ? one(module.argocd_server_iam_role[*].service_account_role_arn) : ""
-      argocd_url                              = local.argocd_ingress_enabled && var.argocd_config["argocd_url"]
+      argocd_url                              = var.argocd_config["argocd_url"]
       admin_password                          = try(one(data.aws_ssm_parameter.encrypted_password[*].value), null)
     }
   )
